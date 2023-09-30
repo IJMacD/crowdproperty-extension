c822b6a420a5629dcc446b98e24eb565f7e7db3f
Iain MacDonald <IJMacD@gmail.com>
2023-09-30 20:42:56 +0800

    Change overdue colour in waterfall chart

--- content-inline.js
+++ content-inline.js
@@ -9,7 +9,18 @@ document.head.append(s);
 function reorderChart () {
     var newOrder = ['Available', 'Pledged', 'Not Started', 'Active Loans', 'Account', '12+ Mths', '6-12 Mths', '3-6 Mths', '0-3 Mths', '0-3 Mths Overdue', '3-6 Mths Overdue', '6-12 Mths Overdue', '12+ Mths Overdue', 'Balance'];
     var d = Highcharts.charts[0].series[0].data;
-    var d2 = newOrder.map(l=>d.find(v=>v.category===l)).filter(x=>x).map(d => ({name:d.name,y:d.y,color:d.color,borderColor:d.borderColor,dataLabels:d.y<0?{style:{color:"white"}}:void 0,isIntermediateSum:d.isIntermediateSum}));
+    const overdueColour = {
+        linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+        stops: [
+            [0, "#bd3654"],
+            [1, "#bd3676"]
+        ]
+    };
+    var d2 = newOrder.map(l=>d.find(v=>v.category===l)).filter(x=>x).map(d => {
+        const color = d.category.includes("Overdue") ? overdueColour : d.color;
+        const dataLabelsColour = (d.y < 0 && d.category != "Balance") ? { style: { color: "white" } } : void 0;
+        return ({name:d.name,y:d.y,color,borderColor:d.borderColor,dataLabels:dataLabelsColour,isIntermediateSum:d.isIntermediateSum});
+    });
     Highcharts.charts[0].update({xAxis:{categories:newOrder}, series:[{name:"Value",data:d2}]}, true);
 }
 
